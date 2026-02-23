7f574d1bffcf36c260d4341dec0e345a6d9f3c1b
Brett Viren <brett.viren@gmail.com>
2026-02-23 18:19:40 -0500

    Attempt to make PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,17 @@ export default function RootLayout({
           src="https://static.cloudflareinsights.com/beacon.min.js"
           data-cf-beacon='{"token": "ef75d67ce4a04caca98866a9a7198357"}'
         ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              if ('serviceWorker' in navigator) {
+                window.addEventListener('load', function() {
+                  navigator.serviceWorker.register('${basePath}/sw.js');
+                });
+              }
+            `,
+          }}
+        />
       </body>
     </html>
   )
