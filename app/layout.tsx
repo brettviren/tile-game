e2468e6e6b49d79b649937186e09240e259fcc7e
Brett Viren <brett.viren@gmail.com>
2026-03-01 16:33:51 -0500

    Hopefully fix some github loading problems

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,10 @@ export default function RootLayout({
     <html lang="en" className="h-full w-full overscroll-none">
       <head>
         <title>ExponenTile</title>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://static.cloudflareinsights.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:; connect-src 'self' https://cloudflareinsights.com https://*.cloudflareinsights.com; worker-src 'self';"
+        />
         <link rel="manifest" href={`${basePath}/manifest.json`} />
         <meta
           name="theme-color"
